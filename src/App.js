c743ae4a367ffa0161a2e14be0783c16887ecf84
Pedro Risso <pmerchiorattor@gmail.com>
2020-09-10 16:53:22 -0300

    to-do-list using map (my first react app)

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const TO_DO_LIST = [
+  'eat',
+  'sleep',
+  'code',
+  'add',
+  'commit',
+  'push',
+  'repeat',
+]
+
+const genertateList = () => TO_DO_LIST.map(task => (`<li>${task}</li>`));
+
+console.log(genertateList());
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (genertateList());
 }
 
 export default App;
